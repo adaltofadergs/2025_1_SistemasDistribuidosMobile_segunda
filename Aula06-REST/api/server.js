930568359b28d620e0eb838d29f4d3cc833005b5
Adalto Selau Sparremberger <adalto.sparremberger@animaeducacao.com.br>
2025-05-19 10:02:51 -0300

    Finalizada a API REST com NodeJS

--- Aula06-REST/api/server.js
+++ Aula06-REST/api/server.js
@@ -43,6 +43,59 @@ const conn = knex( {
         .catch( next )
  })
 
+ app.post( "/produtos" , (req, res, next) => {
+    conn("produto")
+        .insert( req.body )
+        .then(  dados => {
+            if( !dados ){
+                return next( createErrors( 400 , "Não foi possível inserir" ) )
+            }
+            res.status( 201 ).json( { 
+                resposta : "Produto inserido!" ,
+                id : dados[0]
+            } )
+            //res.json( dados ) 
+        } )
+        .catch( next )
+ })
+
+
+ app.put( "/produtos/:idProd" , (req, res, next) => {
+    const id = req.params.idProd
+    conn("produto")
+        .where( "id" , id )
+        .update( req.body )
+        .then( dados => {
+            if( !dados ){
+                return next( createErrors( 404 , "Produto não encontrado") )
+            }
+            res.status(200).json( { resposta: "Produto editado!" } ) 
+        })
+        .catch( next )
+ })
+
+ app.delete( "/produtos/:idProd" , (req, res, next) => {
+    const id = req.params.idProd
+    conn("produto")
+        .where( "id" , id )
+        .delete()
+        .then( dados => {
+            if( !dados ){
+                return next( createErrors( 404 , "Produto não encontrado") )
+            }
+            res.status(200).json( { resposta: "Produto removido!" } ) 
+        })
+        .catch( next )
+ })
+
+app.use( (err, req, res, next ) => {
+    res
+        .status( err.status || 500 )
+        .json( { 
+            erro :  err.message || "Erro interno do Servidor!" 
+        } )
+} ) 
+
  app.listen( PORT , ()=>{
     console.log( `Loja executando em: http://localhost:${PORT}` )
  } )
